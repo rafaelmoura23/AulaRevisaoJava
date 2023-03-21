637b191d034cf3fc271a7fead2a97d27e760749f
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-03-21 14:56:26 -0300

    21.03.2023

--- App.java
+++ App.java
@@ -2,6 +2,8 @@ package AulaRevisao1;
 
 public class App {
     public static void main(String[] args) {
+        ExemploRevisao obj = new ExemploRevisao();
+        obj. exemplo1();
         
     }
     
--- ExemploRevisao.java
+++ ExemploRevisao.java
@@ -1,5 +1,31 @@
 package AulaRevisao1;
 
+import java.util.Scanner;
+
 public class ExemploRevisao {
+    Scanner sc = new Scanner(System.in);
     
+    public void exemplo1() {
+        System.out.println("Informe a 1ª Nota do Aluno:");
+        int nota1 = sc.nextInt();
+        System.out.println("Informe a 2ª Nota do Aluno:");
+        int nota2 = sc.nextInt();
+        double media = (nota1+nota2)/2;
+        System.out.println("A média do aluno é: " +media);
+        System.out.println("Informe o Total de Aulas da Disciplina: ");
+        int aulas = sc.nextInt();
+        System.out.println("Informe o Total de Faltas do Aluno: ");
+        int faltas = sc.nextInt();
+        double frequencia = ((aulas-faltas)*100)/aulas;
+        System.out.println("A frequência do Aluno é: " +frequencia +"%");
+        if(media>=50 && frequencia>=75) {
+            System.out.println("O Aluno está Aprovado");
+        } else if (media<50 && frequencia>=75){
+            System.out.println("O Aluno está de Recuperação");
+        }
+        else{
+            System.out.println("O Aluno está Reprovado");
+        }
+        
+    }
 }
